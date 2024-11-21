1d1174de6cf5f8cc643511b3ca9f656921f96ef2
Amir <amirmohammadbarari415@gmail.com>
2024-11-21 15:26:48 +0330

    complete styling header and make it responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  isFixed: boolean = false;
+  isDropDownVisible: boolean = false;
+  @HostListener('window:scroll', []) onWindowScroll(): void { this.isFixed = window.scrollY > 60 ? true : false; }
 }
